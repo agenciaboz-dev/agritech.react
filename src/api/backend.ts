ef99a9a22b3f767d7fe67a7f35ffff8a70d79f3a
nandobfer <nandobfer@yahoo.com.br>
2023-10-19 16:42:03 -0300

    update: remote backend

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,2 +1,2 @@
-//export const url = "s://app.agenciaboz.com.br:4106"
-export const url = "://192.168.15.8:4106"
+export const url = "s://app.agenciaboz.com.br:4106"
+// export const url = "://192.168.15.8:4106"
