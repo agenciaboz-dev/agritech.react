8e9527ceb2b248337ea8753af367ce961d31e9ff
tainarasn <tainarasn.sba@gmail.com>
2023-10-25 12:37:45 -0300

    feat: docuemntation tab

--- src/pages/Profile/InfoProfile/Documentation.tsx
+++ src/pages/Profile/InfoProfile/Documentation.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material"
+import { Box, TextField } from "@mui/material"
 import React, { ChangeEventHandler } from "react"
+import { textField } from "../../../style/input"
 
 interface DocumentationProps {
     values: FormValues
@@ -7,5 +8,31 @@ interface DocumentationProps {
 }
 
 export const Documentation: React.FC<DocumentationProps> = ({ values, handleChange }) => {
-    return <Box sx={{ flexDirection: "column", gap: "2vw" }}>{values.birth}</Box>
+    return (
+        <Box sx={{ flexDirection: "column", gap: "2vw" }}>
+            <TextField label={"Nome Completo"} name={"name"} sx={textField} value={values.name} onChange={handleChange} />
+            <TextField
+                label={"Data de Nascimento"}
+                name={"birth"}
+                sx={textField}
+                value={values.birth}
+                onChange={handleChange}
+            />
+
+            <TextField
+                label={"Estado Civil"}
+                name={"relationship"}
+                sx={textField}
+                value={values.relationship}
+                onChange={handleChange}
+            />
+            <TextField
+                label={"Nacionalidade"}
+                name={"nacionality"}
+                sx={textField}
+                value={values?.nationality}
+                onChange={handleChange}
+            />
+        </Box>
+    )
 }
