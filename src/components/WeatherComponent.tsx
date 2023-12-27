766b999aab3eb087c7f78f76f9dff8b90225bed2
tainarasn <tainarasn.sba@gmail.com>
2023-12-27 15:34:05 -0300

    feat: finish weather component

--- src/components/WeatherComponent.tsx
+++ src/components/WeatherComponent.tsx
@@ -1,7 +1,13 @@
 import { Box } from "@mui/system"
 import React from "react"
+import sun from "../assets/icons/sun.svg"
 
 interface WeatherComponentProps {}
+
+const style = {
+    fontSize: "3vw",
+    color: "gray",
+}
 
 export const WeatherComponent: React.FC<WeatherComponentProps> = ({}) => {
     return (
@@ -10,14 +16,26 @@ export const WeatherComponent: React.FC<WeatherComponentProps> = ({}) => {
                 height: "18%",
                 flexDirection: "row",
                 justifyContent: "space-between",
-                p: "2vw",
+                alignItems: "center",
+                p: "0vw",
             }}
         >
-            <Box></Box>
-            <Box sx={{ width: "40%", alignItems: "end" }}>
-                <p style={{ fontWeight: "600" }}>Clima</p>
-                <p style={{ fontSize: "3.5vw" }}>Segunda-feira, 15:00</p>
-                <p style={{ fontSize: "3.5vw" }}> Sol</p>
+            <Box sx={{ flexDirection: "row", alignItems: "center", gap: "3vw", width: "70%" }}>
+                <img src={sun} />
+                <Box sx={{ flexDirection: "row", gap: "1vw" }}>
+                    <p style={{ fontSize: "8vw" }}>28</p>
+                    <p style={{ fontSize: "2.8vw", paddingTop: "3vw" }}>°C | °F</p>
+                </Box>
+                <Box>
+                    <p style={style}>Chuva: 4%</p>
+                    <p style={style}>Umidade: 37%</p>
+                    <p style={style}>Vento: 10 km/h</p>
+                </Box>
+            </Box>
+            <Box sx={{ width: "30%", alignItems: "end" }}>
+                <p style={{ fontWeight: "600", fontSize: "3.5vw" }}>Clima</p>
+                <p style={{ fontSize: "2.9vw" }}>Segunda-feira, 15:00</p>
+                <p style={{ fontSize: "3vw" }}> Sol</p>
             </Box>
         </Box>
     )
