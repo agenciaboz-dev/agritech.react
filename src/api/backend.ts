7e34ee7cf833fe7cbe817e80bde54d6b553c003a
tainarasn <tainarasn.sba@gmail.com>
2024-02-06 12:13:27 -0300

    update: prod url

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,3 +1,3 @@
 //export const url = "s://app.agenciaboz.com.br:4106"
-export const url = "://192.168.15.25:4106"
-//export const url = "s://agencyboz.com:4106"
+// export const url = "://192.168.15.25:4106"
+export const url = "s://agencyboz.com:4106"
