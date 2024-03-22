9b78ab2eaeffb0571deb275d4c404af04a418630
tainarasn <tainarasn.sba@gmail.com>
2024-03-22 09:35:37 -0300

    fix: backend

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,3 +1,3 @@
-export const url = "://192.168.15.21:4106"
+// export const url = "://192.168.15.21:4106"
 // export const url = "://192.168.15.5:4106"
-// export const url = "s://agencyboz.com:4106"
+export const url = "s://agencyboz.com:4106"
