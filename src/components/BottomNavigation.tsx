15593bdd8cb6a76c4591e56c258fd936f2accbc8
ispadilha <israel.padilha@gmail.com>
2024-03-28 11:58:49 -0300

    style: adm panel and bottom navigation

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { BottomNavigationAction, BottomNavigation as MuiBottomNav } from "@mui/material"
+import { BottomNavigationAction, BottomNavigation as MuiBottomNav, useMediaQuery } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { colors } from "../style/colors"
 
@@ -9,6 +9,8 @@ interface BottomNavigationProps {
 }
 
 export const BottomNavigation: React.FC<BottomNavigationProps> = ({ external, section }) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
+
     const navigate = useNavigate()
 
     const [currentLocation, setCurrentLocation] = useState<
@@ -34,14 +36,15 @@ export const BottomNavigation: React.FC<BottomNavigationProps> = ({ external, se
             onChange={(_, newValue) => handleChange(newValue)}
             sx={{
                 marginTop: "auto",
-                background: "transparent",
-                padding: "4vw",
-                height: "10vh",
+                background: "white",
+                padding: isMobile ? "4vw" : "1vw",
+                height: isMobile ? "10vh" : "13vh",
                 position: "fixed",
                 left: 0,
                 bottom: 0,
                 justifyContent: "space-between",
                 width: "100%",
+                zIndex: 999,
             }}
         >
             <BottomNavigationAction value={0} sx={{ display: "none" }} />
@@ -49,15 +52,17 @@ export const BottomNavigation: React.FC<BottomNavigationProps> = ({ external, se
                 return (
                     <BottomNavigationAction
                         key={item.id}
-                        label={<p style={{ fontSize: "2.5vw" }}>{item.title}</p>}
+                        label={<p style={{ fontSize: isMobile ? "2.5vw" : "1rem" }}>{item.title}</p>}
                         icon={item.icon}
                         value={item.id}
                         sx={{
                             color: currentLocation?.id == item.id ? "white" : colors.text.black,
                             background: currentLocation?.id == item.id ? colors.text.black : "",
                             borderRadius: "2vw",
-                            gap: "1vw",
-                            padding: "0",
+                            gap: isMobile ? "1vw" : "0.5vw",
+                            padding: "0.5vw",
+                            height: "100%",
+                            minWidth: "2vw",
 
                             "&.Mui-selected": {
                                 color: "white",
