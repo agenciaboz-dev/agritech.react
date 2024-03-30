a643a8729b160f9f0a1a84c36e2c1e018805350a
tainarasn <tainarasn.sba@gmail.com>
2024-03-30 08:22:22 -0300

    feat: call sync

--- src/contexts/callsContext.tsx
+++ src/contexts/callsContext.tsx
@@ -34,7 +34,12 @@ export const CallProvider: React.FC<CallProviderProps> = ({ children }) => {
     const addCallApprove = (newCall: Call) => {
         setCalls((calls) => [...calls, newCall])
     }
-
+    const replaceCall = (data: Call) => {
+        setCalls((list) => [...list.filter((item) => item.id !== data.id), data])
+    }
+    const replaceCallPending = (data: Call) => {
+        setCallsPending((list) => [...list.filter((item) => item.id !== data.id), data])
+    }
     //atualiza a lista de pendentes após aprovação
     const removeCallApprove = (call: Call) => {
         setCallsPending(listCallsPending.filter((item) => item.id !== call.id))
@@ -51,25 +56,62 @@ export const CallProvider: React.FC<CallProviderProps> = ({ children }) => {
     }, [listCallsPending])
 
     useEffect(() => {
-        io.on("call:listApproved:success", (data: Call[]) => {
-            setCalls(data)
+        io.on("call:new", (data: Call) => {
+            if (
+                user?.isAdmin ||
+                data.producerId == user?.producer?.id ||
+                data.kit?.employees?.find((employee) => employee.id == user?.employee?.id)
+            ) {
+                user?.isAdmin ? addCallApprove(data) : addCallPending(data)
+            }
         })
-
-        /** 
-         * ! Exemplo do aprooved call
-         * */
-        io.on("aprovou", (call: Call) => {
+        io.on("call:update", (data: Call) => {
+            if (
+                user?.isAdmin ||
+                data.producerId == user?.producer?.id ||
+                data.kit?.employees?.find((employee) => employee.id == user?.employee?.id)
+            ) {
+                replaceCall(data)
+            }
+        })
+        io.on("call:approved", (call: Call) => {
             if (
                 call.producerId == user?.producer?.id ||
                 call.kit?.employees?.find((employee) => employee.id == user?.employee?.id)
             ) {
-                addCallApprove(call)
+                replaceCall(call)
+            }
+        })
+        io.on("call:closed", (call: Call) => {
+            if (
+                call.producerId == user?.producer?.id ||
+                call.kit?.employees?.find((employee) => employee.id == user?.employee?.id)
+            ) {
+                replaceCall(call)
+            }
+        })
+        io.on("call:cancel", (call: Call) => {
+            if (
+                call.producerId == user?.producer?.id ||
+                call.kit?.employees?.find((employee) => employee.id == user?.employee?.id)
+            ) {
+                replaceCall(call)
             }
         })
 
+        io.on("call:list:approved", (data: Call[]) => {
+            setCalls(data)
+        })
+        io.on("call:list:pending", (data: Call[]) => {
+            setCallsPending(data)
+        })
+
         return () => {
+            io.off("call:new")
+            io.off("call:update")
+            io.off("call:approve")
+            io.off("call:cancel")
             io.off("call:listApproved:success")
-            io.off("aprovou")
         }
     }, [listCalls])
 
