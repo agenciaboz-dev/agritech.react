2693c3bac8ee757ad1a6e38ae01562953624bd64
nandobfer <nandobfer@yahoo.com.br>
2024-04-01 16:38:20 -0300

    feat: storing io emitions on local storage list if client is not connected to the internet and emitting them on reconnect

--- src/contexts/ioContext.tsx
+++ src/contexts/ioContext.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { createContext, useEffect } from "react"
 import { Socket, io as ioSocket } from "socket.io-client"
 import { url } from "../api/backend"
@@ -17,39 +17,55 @@ const IoContext = createContext<IoContextValue>({} as IoContextValue)
 
 export default IoContext
 
-// const io = ioSocket("ws://localhost:4105")
 const io = ioSocket(`ws${url}`)
 
+interface QueuedEvent {
+    eventName: string
+    args: any[]
+}
+
 export const IoProvider: React.FC<IoProviderProps> = ({ children }) => {
+    const socketRef = useRef<Socket | null>(null)
+
     const { snackbar } = useSnackbar()
-    useEffect(() => {
-        io.once("connect_error", (error) => {
-            console.log(error)
-            snackbar({ severity: "error", text: "Não foi possível se conectar com o servidor." })
 
-            api.get("/").then((response) => {
-                console.log(response.data)
-            })
-        })
+    if (!socketRef.current) {
+        const originalSocket = io
 
-        io.on("connect", () => {
-            snackbar({ severity: "success", text: "Conectado!" })
-        })
-
-        io.on("disconnect", (reason) => {
-            if (reason == "io client disconnect" || reason == "io server disconnect") {
-                ;("Desconectado do servidor")
-            } else {
-                ;("Conexão com o servidor perdida! Tentando reconectar automaticamente")
-            }
-        })
-
-        return () => {
-            io.off("connect_error")
-            io.off("connect")
-            io.off("disconnect")
+        const handler = {
+            get(target: any, propKey: string) {
+                if (propKey === "emit") {
+                    return function (eventName: string, ...args: any[]) {
+                        if (!navigator.onLine) {
+                            console.log("sem internet")
+                            snackbar({
+                                severity: "info",
+                                text: "Você está sem conexão. Sua requisição foi salva na lista e será enviada assim que for conectado de novo, não tente novo.",
+                            })
+                            const events = JSON.parse(localStorage.getItem("agritech:queuedEvents") || "[]") as QueuedEvent[]
+                            events.push({ eventName, args })
+                            localStorage.setItem("agritech:queuedEvents", JSON.stringify(events))
+                        } else {
+                            return target.emit(eventName, ...args)
+                        }
+                    }
+                } else {
+                    return target[propKey]
+                }
+            },
         }
-    }, [])
 
-    return <IoContext.Provider value={{ io }}>{children}</IoContext.Provider>
+        socketRef.current = new Proxy(originalSocket, handler)
+
+        originalSocket.on("connect", () => {
+            const events = JSON.parse(localStorage.getItem("agritech:queuedEvents") || "[]") as QueuedEvent[]
+            events.forEach(({ eventName, args }) => {
+                console.log({ eventName, args })
+                originalSocket.emit(eventName, ...args)
+            })
+            localStorage.setItem("agritech:queuedEvents", JSON.stringify([]))
+        })
+    }
+
+    return <IoContext.Provider value={{ io: socketRef.current as Socket }}>{children}</IoContext.Provider>
 }
